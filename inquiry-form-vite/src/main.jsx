1bfa4123cab91ba8b6138c7b1dbef7b6e927466e
ithaksinterlance <it.haksinterlance@gmail.com>
2025-06-03 20:41:22 +0400

    commit

--- inquiry-form-vite/src/main.jsx
+++ inquiry-form-vite/src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = document.getElementById('google-sheet-inquiry-form');
-if (root) {
-  ReactDOM.createRoot(root).render(<App />)
+const container = document.getElementById("google-sheet-inquiry-form");
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
 }
